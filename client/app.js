9e8e43cfed9b5ad1a9661202c41cda2cdfeef5f5
Wesley Delpeut <wesleydelpeut@gmail.com>
2017-11-09 20:07:13 +0100

    Get static data from express instead of angular

--- client/app.js
+++ client/app.js
@@ -1,4 +1,9 @@
 angular.module('helloConference', [])
-    .controller('eventsController', function() {
-        this.events = ['Polymer Summit', 'The Next Web'];
+.controller('eventsController', function($http) {
+    // We only need '/events' because we're serving the javaScript file from our server, localhost, so whatever path
+    // we give it to te http.get call, is going to append that path to the url that we're on.
+    $http.get('/events').then((response) => {
+        this.events = response.data;
     });
+    // this.events = ['Polymer Summit', 'The Next Web'];
+});
--- server/server.js
+++ server/server.js
@@ -2,7 +2,20 @@
 let express = require('express');
 let app = express();
 
+/*
+    This is mounting middleware: static (is build in express core).
+    Argument is the directory express will read files from and serve those static files.
+    Express automatically looks for index.html as your root file.
+*/
 app.use(express.static(__dirname + '/../'));
+
+/*
+    Create end point
+    Test end point using curl: curl -i localhost:8181/events
+ */
+app.get('/events', (request, response) => {
+   response.json(['Polymer Summit', 'The Next Web']);
+});
 
 app.listen(8181, () => {
     console.log('Listening on port 8181...');

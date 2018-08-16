82f27d2720f0f17670fb30320f9d7c98979ce545
Wesley Delpeut <wesleydelpeut@gmail.com>
2018-08-16 22:05:05 +0200

    create end-point in app.js and fix angularJS minification issue regarding to

--- server/server.js
+++ server/server.js
@@ -17,19 +17,20 @@ app.use(express.static(__dirname + '/../client'));
     Create end-point
     Test end-point using curl: curl -i localhost:8181/events
  */
-// app.get('/events', (request, response) => {
-//     let events = mongoUtil.events();
-//     events.find().toArray((err, docs) => {
-//        console.log(JSON.stringify(docs));
-//         /*
-//              Returns the name of the results individually (in an array).
-//              This argument of the map function is the short version of the arrow function and returns for every
-//              element in the collection its name.
-//          */
-//        let eventNames = docs.map((event) => event.name);
-//        response.json(eventNames);
-//     });
-// });
+app.get('/events', (request, response) => {
+    response.json(["Angular Cruise", "Road to React"]);
+    // let events = mongoUtil.events();
+    // events.find().toArray((err, docs) => {
+    //    console.log(JSON.stringify(docs));
+    //     /*
+    //          Returns the name of the results individually (in an array).
+    //          This argument of the map function is the short version of the arrow function and returns for every
+    //          element in the collection its name.
+    //      */
+    //    let eventNames = docs.map((event) => event.name);
+    //    response.json(eventNames);
+    // });
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');

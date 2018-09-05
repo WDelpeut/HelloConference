783968f6a2e3753678eb8c5d9b7de674119e9d12
Wesley Delpeut <wesleydelpeut@gmail.com>
2018-09-05 21:00:33 +0200

    Replace static data for a get request to the backend

--- client/app.js
+++ client/app.js
@@ -26,35 +26,8 @@ angular.module('helloConference', [uiRouter])
         url: '/:eventName',
         templateUrl: 'events/events-details.html',
         resolve: {
-          // $q is Angular's imlementation of a promise
-          eventService: function ($q) {
-            return $q((resolve, reject) => {
-              let event = {
-                "name": "VueJS Amsterdam 2019",
-                "date": [
-                  "2019-02-14",
-                  "2019-02-15"
-                ],
-                "address": {
-                  "country": "the Netherlands",
-                  "city": "Amsterdam",
-                  "postal-code": "1013AP",
-                  "streetAddress": "Danzigerkade 5"
-                },
-                "venue": "Theater Amsterdam",
-                "URL": "https://www.vuejs.amsterdam/",
-                "topics": [
-                  "vue"
-                ],
-                "speakers": [
-                  "Evan You",
-                  "Sara Vieira",
-                  "Filipa Lacerda",
-                  "Jen Looper"
-                ]
-              }
-              resolve({ data: event });
-            })
+          eventService: function ($http, $stateParams) {
+            return $http.get('/events/' + $stateParams.eventName)
           }
         },
         controller: function (eventService) {
